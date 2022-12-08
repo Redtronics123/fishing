11dd985ec719ca5bc00d34a4213fcaf8991023a3
Redtronics <development@redtronics.de>
2022-12-08 08:46:39 +0100

    add log4j and apache poi

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.xerial:sqlite-jdbc:3.40.0.0")
+    implementation("org.apache.poi:poi-ooxml:5.2.3")
+    implementation("org.apache.logging.log4j:log4j-core:2.19.0")
 }
 
 tasks.test {
